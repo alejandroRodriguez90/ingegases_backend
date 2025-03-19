7d79d73519374a5c3d81f9484a0d5886ed401777
cantillo <juanjose.cantillo@dedalus.eu>
2025-03-19 00:19:43 -0500

    :bug: rol id

--- apps/user/views.py
+++ apps/user/views.py
@@ -12,22 +12,29 @@ class UsuarioView (APIView):
         return Response(usuarios.data)
 
     def post(self, request):
-        # Obtener el rol con ID 2
         try:
+            # Obtener el rol con ID 2
             rol = Rol.objects.get(id=2)
         except Rol.DoesNotExist:
             return Response({"error": "El rol con ID 2 no existe"}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Crear el usuario con el rol predefinido
+        # Copiar request.data para modificarlo
         usuario_data = request.data.copy()
-        usuario_data['rol'] = rol.id
 
-        usuario = UsuarioSerializers(data=usuario_data)
-        if usuario.is_valid():
-            usuario.save(rol=rol)
-            return Response(usuario.data, status=status.HTTP_201_CREATED)
+        # Crear el usuario con el rol asignado
+        usuario = Usuario(
+            nombre=usuario_data['nombre'],
+            correo=usuario_data['correo'],
+            usuario=usuario_data['usuario'],
+            contrasena=usuario_data['contrasena'],
+            rol_id=rol
+        )
 
-        return Response(usuario.errors, status=status.HTTP_400_BAD_REQUEST)
+        usuario.save()
+
+
+        usuario_serializado = UsuarioSerializers(usuario)
+        return Response(usuario_serializado.data, status=status.HTTP_201_CREATED)
 
 
 
